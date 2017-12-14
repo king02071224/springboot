7646f0d3e18a8fa8c0f1da02851b9ca1ef260401
king27 <m17671289358@163.com>
2017-12-15 01:16:34 +0800

    修改bb文件

--- src/main/java/cn/wolfcode/springdemo/SpringdemoApplication.java
+++ src/main/java/cn/wolfcode/springdemo/SpringdemoApplication.java
@@ -8,5 +8,7 @@ public class SpringdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoApplication.class, args);
+		System.out.println("添加打印功能");
+
 	}
 }
