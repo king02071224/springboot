ea3fb414d8adc53f315bacefdff6698d73bc1552
king02071224 <34536625+king02071224@users.noreply.github.com>
2017-12-15 01:17:31 +0800

    Update SpringdemoApplication.java

--- src/main/java/cn/wolfcode/springdemo/SpringdemoApplication.java
+++ src/main/java/cn/wolfcode/springdemo/SpringdemoApplication.java
@@ -9,6 +9,7 @@ public class SpringdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoApplication.class, args);
 		System.out.println("添加打印功能");
+		//4444
 
 	}
 }
